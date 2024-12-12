337e5b60f5dd996c443c370c54aec558557ccbaf
Stanislav Saveliev <texnikum2288@gmail.com>
2024-12-12 21:14:27 +0300

    Добавьте файлы проекта.

---
+++ Calc 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double onenumber, twonumber;
+                string simvol;
+
+                Console.WriteLine("Введите число 1");
+                onenumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число 2");
+                twonumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию: '+','-','*','/'");
+                simvol = Console.ReadLine();
+
+                if (simvol == "+")
+                {
+                    Console.WriteLine(onenumber + twonumber);
+                }
+                else if (simvol == "-")
+                {
+                    Console.WriteLine(onenumber - twonumber);
+                }
+                else if (simvol == "*")
+                {
+                    Console.WriteLine(onenumber * twonumber);
+                }
+                else if (simvol == "/")
+                {
+                    if (twonumber == 0)
+                    {
+                        Console.WriteLine("Ошибка операции!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(onenumber / twonumber);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Выбрана не верная операция!");
+                }
+                Console.ReadLine();
+            }          
+
+        }
+    }
+}
---
+++ Calculator 1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Console.Clear();
+                double onenumber, twonumber;
+                string simvol;
+
+                try
+                {
+                    Console.WriteLine("Введите число 1");
+                    onenumber = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите число 2");
+                    twonumber = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не верно введены данные!");
+                    Console.ReadLine();
+                    continue;
+                }                
+
+                Console.WriteLine("Выберите операцию: '+','-','*','/'");
+                simvol = Console.ReadLine();
+
+                switch (simvol)
+                {
+                    case "+":
+                        Console.WriteLine(onenumber + twonumber);
+                        break;
+
+                    case "-":
+                        Console.WriteLine(onenumber - twonumber);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(onenumber * twonumber);
+                        break;
+
+                    case "/":
+                        if (twonumber == 0)
+                        {
+                            Console.WriteLine("Ошибка операции!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(onenumber / twonumber);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка операции!");
+                        break;
+                        
+                }
+                Console.ReadLine();
+
+            
+            }
+        }
+        
+    }
+}
